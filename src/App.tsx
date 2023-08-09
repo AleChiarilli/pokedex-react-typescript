2f57d57f8e81d29670ef21057ff0396f8458a5d4
AleChiarilli <alejandrochiarilli@gmail.com>
2023-08-09 18:45:57 +0200

    feat request and display pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import { PokedexEntry } from './components/PokedexEntry';
 
-type Pokemon = {
+export type Pokemon = {
   url: string;
   name: string;
-  types: string;
+  id?: string;
 };
 
 function capitalizeFirstLetter(str: string) {
@@ -17,11 +18,14 @@ function convertDigits(num: number, width: number) {
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [selectedPokemon, setSelectedPokemon] = useState('');
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon>({
+    url: '',
+    name: '',
+  });
 
   const callPokeApi = async () => {
     const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=150`,
+      `https://pokeapi.co/api/v2/pokemon?limit=151`,
       {
         method: 'GET',
         headers: {
@@ -37,30 +41,31 @@ function App() {
     callPokeApi().catch(console.error);
   }, []);
 
-  const handleSelectedPokemon = (pokemonId: number) => {
-    setSelectedPokemon(pokemonId.toString()); // Cambiar el estado de false a true
+  const handleSelectedPokemon = (pokemon: Pokemon) => {
+    setSelectedPokemon(pokemon); // Cambiar el estado de false a true
   };
 
   return (
     <>
-      <h1>Pokemon</h1>
-      <p>{selectedPokemon}</p>
-      {/* <div className="card">
-        {pokemons.map((item, i) => (
-          <a href={item.url} key={i}>{convertDigits(i + 1, 3)} {capitalizeFirstLetter(item.name)}</a> */}
+      <h1>AlexDex</h1>
+      {selectedPokemon.id && (
+        <section>
+          <PokedexEntry pokemon={selectedPokemon} />
+        </section>
+      )}
       <ul className="grid gap-4 grid-cols-3 grid-rows-3">
-        {pokemons.map((item, i) => (
+        {pokemons.map((pokemon, index) => (
           <li
-            key={i}
-            onClick={() => handleSelectedPokemon(i + 1)}
-            className="hover:cursor-pointer flex flex-col items-center hover:bg-slate-50 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            key={pokemon.name}
+            onClick={() =>
+              handleSelectedPokemon({ ...pokemon, id: `${index + 1}` })
+            }
+            className="hover:cursor-pointer p-5 flex flex-col items-center hover:bg-slate-50 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <section className="flex flex-col items-center p-5">
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {capitalizeFirstLetter(item.name)}
-              </h2>
-              <p>{convertDigits(i + 1, 3)}</p>
-            </section>
+            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {capitalizeFirstLetter(pokemon.name)}
+            </p>
+            <p>{convertDigits(index + 1, 3)}</p>
           </li>
         ))}
       </ul>
--- src/components/PokedexEntry.tsx
+++ src/components/PokedexEntry.tsx
@@ -1,10 +1,96 @@
-import { FC } from 'react';
-import './App.css';
+import { FC, useEffect, useState } from 'react';
+import { Pokemon } from '../App';
 
-type Props = {
-  pokemonId: string;
+type FlavorTextEntry = {
+  flavor_text: string;
 };
 
-export const PokedexEntry: FC<Props> = ({ pokemonId }) => {
-  return <>{pokemonId}</>;
+type PokedexFlavorTextEntry = {
+  flavor_text_entries: FlavorTextEntry[];
+};
+
+type PokemonSpecies = {
+  url: string;
+};
+
+type PokemonStat = {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+};
+
+type PokemonType = {
+  slot: number;
+  type: {
+    name: string;
+  };
+};
+
+type PokemonDetail = {
+  sprites: {
+    front_default: string;
+  };
+  types: PokemonType[];
+  stats: PokemonStat[];
+  species: PokemonSpecies;
+};
+
+type Props = {
+  pokemon: Pokemon;
+};
+
+export const PokedexEntry: FC<Props> = ({ pokemon }) => {
+  const [pokemonDetails, setPokemonDetails] = useState<PokemonDetail | null>(
+    null,
+  );
+
+  const [pokemonFlavor, setPokemonFlavor] =
+    useState<PokedexFlavorTextEntry | null>(null);
+
+  useEffect(() => {
+    fetch(pokemon.url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(setPokemonDetails)
+      .catch(console.error);
+  }, [pokemon]);
+
+  useEffect(() => {
+    if (pokemonDetails) {
+      fetch(pokemonDetails.species.url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(res => res.json())
+        .then(setPokemonFlavor)
+        .catch(console.error);
+    }
+  }, [pokemonDetails]);
+
+  // pokemonDetails.species.url
+
+  return (
+    <>
+      <div className="border-2 border-red-600 border-dashed rounded">
+        {pokemon.id} {pokemon.name} {pokemon.url}
+        {pokemonDetails && (
+          <>
+            <img src={pokemonDetails.sprites.front_default}></img>
+            <div>{pokemonDetails.types[0].type.name}</div>
+            {pokemonDetails.types[1] && (
+              <div>{pokemonDetails.types[1].type.name}</div>
+            )}
+            <div>{pokemonFlavor?.flavor_text_entries[0].flavor_text}</div>
+          </>
+        )}
+      </div>
+    </>
+  );
 };

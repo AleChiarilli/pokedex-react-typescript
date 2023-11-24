f0de9173400a66f655aeffe06e3a47c9ba62e636
Alejandro Chiarilli <125934002+AleChiarilli@users.noreply.github.com>
2023-11-24 21:57:45 +0000

    colors by type sended to constants.ts

--- src/pages/PokedexEntries/components/StatProgressBar.tsx
+++ src/pages/PokedexEntries/components/StatProgressBar.tsx
@@ -1,24 +1,6 @@
 import { FC } from 'react';
 import { PokemonType } from './PokeBarType';
-
-const colorsByType = {
-  grass: 'bg-green-600',
-  poison: 'bg-purple-600',
-  fire: 'bg-red-600',
-  flying: 'bg-orange-100',
-  water: 'bg-sky-400',
-  bug: 'bg-lime-700',
-  normal: 'bg-neutral-400',
-  fairy: 'bg-pink-300',
-  ground: 'bg-yellow-900',
-  fighting: 'bg-orange-600',
-  psychic: 'bg-pink-800',
-  rock: 'bg-amber-900',
-  steel: 'bg-teal-700',
-  electric: 'bg-yellow-300',
-  ghost: 'bg-purple-800',
-  dragon: 'bg-purple-500',
-};
+import { colorsByType } from '../../../utils/constants';
 
 export const StatProgressBar: FC<{
   value: number;
